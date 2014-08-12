c34be6ba963ea2951f2cffb94be4e02ddc0bcad5
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-08-12 14:21:59 -0400

    added migrations for lat/long and cord1/cord2 to restaurant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,33 @@
 require 'csv'
-file = File.expand_path('../../public/WebExtract2.csv', __FILE__)
-CSV.parse(File.open(file, 'r:iso-8859-1:utf-8'){|f| f.read}, col_sep: ',', headers: true) do |row|
-	row = row.to_hash
+# file = File.expand_path('../../public/WebExtract2.csv', __FILE__)
+# CSV.parse(File.open(file, 'r:iso-8859-1:utf-8'){|f| f.read}, col_sep: ',', headers: true) do |row|
+# 	row = row.to_hash
 
-	r = Restaurant.new(
-		name: row["DBA"],
-		boro: row["BORO"],
-		building: row["BUILDING"],
-		street: row["STREET"],
-		zipcode: row["ZIPCODE"],
-		phone: row["PHONE"],
-		cuisine_code: row["CUISINECODE"]
-		)
-r.save
+# 	r = Restaurant.new(
+# 		name: row["DBA"],
+# 		boro: row["BORO"],
+# 		building: row["BUILDING"],
+# 		street: row["STREET"],
+# 		zipcode: row["ZIPCODE"],
+# 		phone: row["PHONE"],
+# 		cuisine_code: row["CUISINECODE"]
+# 		)
+# r.save
 
 
-i = Inspection.create(
-	phone: row["PHONE"],
-	inspection_date: row["INSPDATE"],
-	action: row["ACTION"],
-	violation_code: row["VIOLCODE"],
-	score: row["SCORE"],
-	current_grade: row["CURRENTGRADE"],
-	grade_date: row["GRADEDATE"],
-	record_date: row["RECORDDATE"]
-	)
+# i = Inspection.create(
+# 	phone: row["PHONE"],
+# 	inspection_date: row["INSPDATE"],
+# 	action: row["ACTION"],
+# 	violation_code: row["VIOLCODE"],
+# 	score: row["SCORE"],
+# 	current_grade: row["CURRENTGRADE"],
+# 	grade_date: row["GRADEDATE"],
+# 	record_date: row["RECORDDATE"]
+# 	)
 
-end
-
+# end
+ 																												
 # r.each do |rest|
 # 	i.each do |insp|
 # 		if rest.phone == insp.phone
@@ -35,7 +35,7 @@ end
 # 		end
 # 	end
 # end
-i.each do |ins|
+Inspection.where(restaurant_id: nil).find_each(batch_size: 5000) do |ins|
 	r = Restaurant.find_by(phone: ins.phone)
 	r.inspections << ins
 	p r
