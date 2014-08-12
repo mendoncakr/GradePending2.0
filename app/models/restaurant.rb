fe65eb6e2ff5582cfa314f9576a238cc0af18bfd
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-08-12 14:25:44 -0400

    added method for saving latitude and longitude to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,18 @@
 class Restaurant < ActiveRecord::Base
   has_many :inspections
   validates_uniqueness_of :phone
+  geocoded_by :address
+
 
   def address
   	"#{self.building} " + "#{self.street} " + "#{self.zipcode}"
+  end
+
+  def save_coords
+  	coords = Geocoder.coordinates(self.address)
+  	self.coord1 = coords[0]
+  	self.coord2 = coords[1]
+  	self.save
   end
 
 end
