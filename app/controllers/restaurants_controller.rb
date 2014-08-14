c5dccacfaae7166ebdb9cb4a47be9adffa03e701
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-08-14 15:26:33 -0400

    gogle maps api instead of gmaps4rails

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,13 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.manhattan
-    @hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
-      @restaurant = restaurant.phone
-      marker.lat restaurant.latitude
-      marker.lng restaurant.longitude
-      marker.infowindow render_to_string(:partial => "/restaurants/info", :locals => { :object => @restaurant})
-      marker.json({:name=> restaurant.name})
-    end
+    # @restaurants = Restaurant.manhattan
+    # @hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
+    #   @restaurant = restaurant.phone
+    #   marker.lat restaurant.latitude
+    #   marker.lng restaurant.longitude
+    #   marker.infowindow render_to_string(:partial => "/restaurants/info", :locals => { :object => @restaurant})
+    #   marker.json({:name=> restaurant.name})
+    # end
 
     # client = SODA::Client.new({:domain => "explore.data.gov", :app_token => ENV['SODA_API_KEY']})
     # p resonse = client.get("4vkw-7nck?")
