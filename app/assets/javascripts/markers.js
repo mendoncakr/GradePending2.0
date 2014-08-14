991ea6f250586eba7d536bd1c7471956ac496011
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-08-14 18:29:13 -0400

    dots on the map - need to optimize for performance, but overall load time seems a bit faster than w/ gmaps4rails

--- app/assets/javascripts/markers.js
+++ app/assets/javascripts/markers.js
@@ -1,25 +1,42 @@
 var getCoords = function() {
 	console.log('yolo')
-		$.ajax({
+	return $.ajax({
 		url: '/markers.json',
 		method: 'get',
 		success: function(resp) {
+
 			console.log(resp.restaurants)
-			for ( var i =0; i < resp.restaurants.length; i++){
-				var marker_i = new google.maps.Marker({
-					position: new google.maps.LatLng(resp.restaurants[i][0], resp.restaurants[i][1])
-				})
-				console.log(marker_i)
+			
+			function initialize() {
+				var mapOptions = {
+					center: new google.maps.LatLng(40.7731024, -73.872255),
+					zoom: 12
+				};
+				var map = new google.maps.Map(document.getElementById("map-canvas"),
+					mapOptions);
+
+				var infowindow = new google.maps.InfoWindow({
+					content: 'YOLOYLOYLYOYLYOLYOYLYOYLYOYLYO'
+				});
+				var that = map;
+				for ( var i =0; i < resp.restaurants.length; i++) {
+					var marker = new google.maps.Marker({
+						position: new google.maps.LatLng(resp.restaurants[i][0], resp.restaurants[i][1]),
+						map : map
+					});
+				}	
 			}
-		console.log('hey');
-		},
-		fail: function(resp) {
-			console.log(resp)
+			initialize();
 		}
 	});
 }
 
+
+
+
 $(function() {
 	getCoords();
+	getCoords().done(function() {
+	});	
 	
 });
