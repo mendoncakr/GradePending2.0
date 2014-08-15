5bf6c33d2a1a8721b1762eaaa4d6247286ae2db4
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-08-15 15:38:13 -0400

    link to restaurant show page added to infobox

--- app/assets/javascripts/markers.js
+++ app/assets/javascripts/markers.js
@@ -20,14 +20,11 @@ var getCoords = function() {
 						position: new google.maps.LatLng(resp.restaurants[i][1], resp.restaurants[i][2]),
 						map : map
 					});
-					marker.info = resp.restaurants[i][0]
-					
-					
-					// var infowindow = new google.maps.InfoWindow({});
+					marker.info = { name : resp.restaurants[i][0], id: resp.restaurants[i][3]}
 
 				google.maps.event.addListener(marker, 'click', (function(marker, i) {
 						return function() {
-							infowindow.setContent('<bold>Name: '+marker.info +'<br></bold>')
+							infowindow.setContent('<bold>Name: '+'<a href="/restaurants/'+marker.info.id+'">'+marker.info.name +'</a></bold>')
 							infowindow.open(map, marker);
 						}
 					})(marker, i));
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,16 +1,8 @@
 class RestaurantsController < ApplicationController
   def index
-    # @restaurants = Restaurant.manhattan
-    # @hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
-    #   @restaurant = restaurant.phone
-    #   marker.lat restaurant.latitude
-    #   marker.lng restaurant.longitude
-    #   marker.infowindow render_to_string(:partial => "/restaurants/info", :locals => { :object => @restaurant})
-    #   marker.json({:name=> restaurant.name})
-    # end
+  end
 
-    # client = SODA::Client.new({:domain => "explore.data.gov", :app_token => ENV['SODA_API_KEY']})
-    # p resonse = client.get("4vkw-7nck?")
+  def show
   end
 end
 
