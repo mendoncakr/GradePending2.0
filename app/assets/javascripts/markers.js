cb121b47a206f2ab71d5baa43788d940048cf11d
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-08-15 20:50:44 -0400

    fusion table layer instead of ajax request for improved load time

--- app/assets/javascripts/markers.js
+++ app/assets/javascripts/markers.js
@@ -1,39 +1,22 @@
 var getCoords = function() {
-	console.log('yolo')
-	return $.ajax({
-		url: '/markers.json',
-		method: 'get',
-		success: function(resp) {
+	function initialize() {
+		var mapOptions = {
+			center: new google.maps.LatLng(40.766579, -73.9783445),
+			zoom: 14
+		};
 
-			console.log(resp.restaurants)
-			
-			function initialize() {
-				var mapOptions = {
-					center: new google.maps.LatLng(40.766579, -73.9783445),
-					zoom: 14
-				};
-				var map = new google.maps.Map(document.getElementById("map-canvas"),
-					mapOptions);
-				var infowindow = new google.maps.InfoWindow({});
-				for ( var i =0; i < resp.restaurants.length; i++) {
-					var marker = new google.maps.Marker({
-						position: new google.maps.LatLng(resp.restaurants[i][1], resp.restaurants[i][2]),
-						map : map
-					});
-					marker.info = { name : resp.restaurants[i][0], id: resp.restaurants[i][3]}
-
-				google.maps.event.addListener(marker, 'click', (function(marker, i) {
-						return function() {
-							infowindow.setContent('<bold>Name: '+'<a href="/restaurants/'+marker.info.id+'">'+marker.info.name +'</a></bold>')
-							infowindow.open(map, marker);
-						}
-					})(marker, i));
-				}
-					
+		var layer2 = new google.maps.FusionTablesLayer({
+			query: {
+				select: 'name',
+				from: '1Tb6rqCR-YqIrvfJ07wIkH0-QFnE26b3U3hriuYkq'
 			}
-			initialize();
-		}
-	});
+		});
+		
+		var map = new google.maps.Map(document.getElementById("map-canvas"),
+			mapOptions);
+		layer2.setMap(map);
+	}
+	initialize();
 }
 
 
