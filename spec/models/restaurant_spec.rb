68c4d05d90606d79f09f2534030fb02176a9b654
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-08-16 11:49:20 -0400

    added test for fix_name method

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Restaurant, :type => :model do
 	it "creates a valid restaurant" do 
 		expect(restaurant).to be_valid
 	end
+
+	context '#address'
+
+		it "removes trailing whitespace" do 
+			@restaurant = FactoryGirl.build(:restaurant)
+			expect(@restaurant.address).to eq "42 EAST   20 STREET   10003"
+		end
+
+	context '#name'
+
+		it "replaces &amp;" do 
+			@restaurant = FactoryGirl.build(:restaurant, name: "Joe's Pizza &amp; Shakes")
+			expect (@restaurant.fix_name).to eq "Joe's Pizza & Shakes"
+		end
+
+
 end
