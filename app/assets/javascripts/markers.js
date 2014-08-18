d9649a1c71a450d248b91998d6638e306745ebbc
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-08-18 08:16:12 -0400

    403 forbidden in fusiontips callback

--- app/assets/javascripts/markers.js
+++ app/assets/javascripts/markers.js
@@ -15,6 +15,29 @@ var getCoords = function() {
 		var map = new google.maps.Map(document.getElementById("map-canvas"),
 			mapOptions);
 		layer2.setMap(map);
+
+		layer2.enableMapTips({
+      		select: "'name'", // list of columns to query, typially need only one column.
+      		from: '1Tb6rqCR-YqIrvfJ07wIkH0-QFnE26b3U3hriuYkq', // fusion table name
+          geometryColumn: 'latitude', // geometry column name
+      		suppressMapTips: false, // optional, whether to show map tips. default false
+          delay: 200, // milliseconds mouse pause before send a server query. default 300.
+          tolerance: 8 // tolerance in pixel around mouse. default is 6.
+      	});
+              //listen to events if desired.
+              google.maps.event.addListener(layer2, 'mouseover', function(fEvent) {
+              	var row = fEvent.row;
+              	myHtml = 'mouseover:<br/>';
+              	for (var x in row) {
+              		if (row.hasOwnProperty(x)) {
+              			myHtml += '<b>' + x + "</b>:" + row[x].value + "<br/>";
+              		}
+              	}
+              	document.getElementById('info').innerHTML = myHtml;
+              });
+              google.maps.event.addListener(layer, 'mouseout', function(fevt) {
+              	document.getElementById('info').innerHTML = '';
+              });
 	}
 	initialize();
 }
