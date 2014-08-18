981dacd3932e6436c72c2b9e500932a9f7be12b7
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-08-18 11:31:00 -0400

    adding model associations. Users can favorite restaurants and users who favorited restaurants can be viewed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :favorite_restaurants
+  has_many :favorites, through: :favorite_restaurants, source: :restaurant
 end
