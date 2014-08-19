9a0863add612f2cb2739fbb4e5b1197c3a8d7965
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-08-19 16:07:24 -0400

    WIP: AJAX for adding restaurant to favorites. Need to fix google maps error of 'cannot read property of 'offsetwidth' of null. div of id map_canvas can be added as work around

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -4,26 +4,26 @@
 // 	}); 
 // }
 
-var addFavorite = function() {
-	$.ajax({
-		url: '/restaurants/:id/favorite',
-		method: "POST"
-	}).success(function(response){
-		console.log("SUCCESSFULLY added restaurant as a favoite! :) ");
-		console.log(response);
-	}).fail(function(response){
-		console.log("FAILED to add restaurant as favorite :( ");
-		console.log(response);
-	});
-}
+// var addFavorite = function() {
+// 	$.ajax({
+// 		url: '/restaurants/:id/favorite',
+// 		method: "POST"
+// 	}).success(function(response){
+// 		console.log("SUCCESSFULLY added restaurant as a favoite! :) ");
+// 		console.log(response);
+// 	}).fail(function(response){
+// 		console.log("FAILED to add restaurant as favorite :( ");
+// 		console.log(response);
+// 	});
+// }
 
-$(document).ready(function(){
-	console.log("Working!");
+// // $(document).ready(function(){
+// // 	console.log("Working!");
 
-	$("#add_favorite").on("click", function(e){
-		e.preventDefault();
-		console.log("CLICK!")
-		addFavorite();
-	})
+// // 	$("#add_favorite").on("click", function(e){
+// // 		e.preventDefault();
+// // 		console.log("CLICK!")
+// // 		addFavorite();
+// // 	})
 
-});
+// // });
