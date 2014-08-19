cc4991d926b75eeb4532df26c9d1aa0cd60e9769
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-08-19 19:45:43 -0400

    working AJAX for adding restaurant to favorites list

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -1,29 +1,28 @@
-// var restaurant = function(){ 
-// 	return $.get('/restaurants/:id.json', function(response){
-// 		console.log(response);
-// 	}); 
-// }
+var authenticityToken =  function(xhr) { xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content')) }
 
-// var addFavorite = function() {
-// 	$.ajax({
-// 		url: '/restaurants/:id/favorite',
-// 		method: "POST"
-// 	}).success(function(response){
-// 		console.log("SUCCESSFULLY added restaurant as a favoite! :) ");
-// 		console.log(response);
-// 	}).fail(function(response){
-// 		console.log("FAILED to add restaurant as favorite :( ");
-// 		console.log(response);
-// 	});
-// }
+var addFavorite = function(id) {
+	$.ajax({
+		url: '/restaurants/'+ id +'/favorite',
+		beforeSend: authenticityToken,
+		method: "POST",
+	}).success(function(response){
+		console.log("SUCCESSFULLY added restaurant as a favorite! :) ");
+		console.log(response);
+	}).fail(function(response){
+		console.log("FAILED to add restaurant as favorite :( ");
+			console.log(response);
+		});
+}
 
-// // $(document).ready(function(){
-// // 	console.log("Working!");
+$(function(){
+	console.log("Working!");
 
-// // 	$("#add_favorite").on("click", function(e){
-// // 		e.preventDefault();
-// // 		console.log("CLICK!")
-// // 		addFavorite();
-// // 	})
+	$("#add_favorite").on("click", function(e){
+		var restaurantID = $('#add_favorite').data("restaurant")  // Setting restaurant ID here for now, need to set up even delegation as we are currently binding to element before it is created in the DOM
+		e.preventDefault();	
+		addFavorite(restaurantID);
+	})
 
-// // });
+});
+
+
