91fd0ccf05d8a04dee194ff8eddd4e1442b5058e
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-08-27 01:48:17 -0400

    deleted unused controller

--- app/controllers/restaruant_map_controller.rb
+++
@@ -1,7 +0,0 @@
-RestaurantMapController < ApplicationController
-	
-	def find
-		@restuarant = Restaurant.find(params[:id])
-		{latitude: @restuarant.latitude longitude: @resturant.longitude}.to_json
-	end
-end
