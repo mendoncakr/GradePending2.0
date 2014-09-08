c5a441f355182dc892d0b8db8a2fa97118953a4e
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-09-08 16:33:06 -0400

    disabled highcharts.com credit

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -5,42 +5,47 @@ function gradesAjax (callback) {
   })
 }
 $(function () {
-  gradesAjax(function(response) {
-    $('#container').highcharts({
-      chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: 1,//null,
-        plotShadow: false
-      },
-      title: {
-        text: 'Breakdown of Grades in New York City Restaurants'
-      },
-      tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-      },
-      plotOptions: {
-        pie: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          dataLabels: {
-            enabled: true,
-            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-            style: {
-              color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+  if (document.getElementById('container') !== null) {
+    gradesAjax(function(response) {
+      $('#container').highcharts({
+        chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: 1,//null,
+          plotShadow: false
+        },
+        title: {
+          text: 'Breakdown of Grades in New York City Restaurants'
+        },
+        tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        credits: {
+          enabled: false
+        },
+        plotOptions: {
+          pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+              enabled: true,
+              format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+              style: {
+                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+              }
             }
           }
-        }
-      },
-      series: [{
-        type: 'pie',
-        name: 'Restaurant Inspection Grade',
-        data: [
-        ['A',   response.a],
-        ['B',       response.b],
-        ['C',    response.c],
-        ]
-      }]
-    });
+        },
+        series: [{
+          type: 'pie',
+          name: 'Restaurant Inspection Grade',
+          data: [
+          ['A',   response.a],
+          ['B',       response.b],
+          ['C',    response.c],
+          ]
+        }]
+      });
 
-  })
+    })
+  }
 }); 
