1878492728d189d401b51accc28b8890cc4ae362
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-09-09 02:50:02 -0400

    more descriptive file name for search
    separation of concerns is key here

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,38 @@
+function namesAjax (callback) {
+	$.get('restaurants/index.json')
+	.done(function (data) {
+		callback (data)
+	});
+}		
+
+$(function() {
+  $(document).one("load", function(){ namesAjax(); })
+   
+  var searchWrapper = (function () {
+    var that = this;
+
+    namesAjax(function (response) {
+      var namesAndIds = response;
+      var sr =  $.map(namesAndIds, function (key, value) {
+       return {
+        label : value,
+        value : value,
+        id: key
+      };
+    });
+      $('#search').autocomplete({source: sr, change: function (event, ui) {
+        that.id = ui.item.id;
+        console.log(that.id)   
+      }})
+
+    });
+
+    $('.search').on('submit', function (e) {
+     e.preventDefault()
+     window.location = "/restaurants/"+ that.id
+   })
+  })();
+});	
+
+	
+
