a0c600ad74e1453c83f9ef8c22bc7dce3e58522c
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-09-12 00:47:49 -0400

    added cache for inspection violations

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -1,3 +1,7 @@
 class Inspection < ActiveRecord::Base
   belongs_to :restaurant
+
+  def self.violation_cache
+  	Rails.cache.fetch('violation_code') { all.pluck(:violation_code).flatten }
+  end
 end
