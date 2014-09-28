e54c8c829d1c5b7e2e40ec10fa641b33569d6c15
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-09-28 15:13:30 -0400

    removed unneeded yelp restaurant model tests

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -24,34 +24,4 @@ RSpec.describe Restaurant, :type => :model do
 		expect(new_restaurant.name).to eq "Joe's Pizza & Shakes"
 	end
 
-	context '#Yelp'
-	it "retrieves the yelp business id from Yelp search API" do
-			# restaurant =  FactoryGirl.build(:restaurant)
-			VCR.use_cassette 'model/yelp_search_api' do
-				response = restaurant.yelp_biz_id 
-				expect(response).to eq("gramercy-tavern-new-york")
-			end
-		end
-
-		it "returns an excerpt of the top review" do
-      VCR.use_cassette 'model/yelp_business_api' do
-  			response = restaurant.top_yelp_review
-  			expect(response).to eq("Pretty expensive, but worth it. Little bit of an older crowd, but worth it. \n\nI've only eaten in The tavern area with colleagues, so haven't tried the fixed...")
-		end
-  end
-
-		it "returns the user for the yelp review " do
-      VCR.use_cassette 'model/yelp_business_api' do
-			 response = restaurant.yelp_review_user
-			 expect(response).to eq("John S.")
-		  end
-    end
-
-		it "returns the url for the owner of the reviews profile picture" do
-      VCR.use_cassette 'model/yelp_business_api' do
-			 response = restaurant.yelp_review_user_image
-			 expect(response).to eq("http://s3-media4.fl.yelpcdn.com/photo/yFUKeS--E8CkErS-1dryoQ/ms.jpg" )
-		  end
-    end
-
-	end
+end
